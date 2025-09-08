983fad6d8b3186cda9aba0d06979cb92d5adc8c9
Ashrox102 <nathanielashirvad@gmail.com>
2025-09-09 04:37:07 +0530

    virtusa practice

---
+++ recursion/fibonacci.py
@@ -0,0 +1,18 @@
+def fibo(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        print()
+        return fibo(num-1) + fibo(num-2)
+    
+a = int(input("enter how much fibo"))
+
+for i in range (a):
+    print(fibo(i))
+
+    
+    
+
+
